67c5b526be0ba2c30233fb5e7b88f454360f3a42
Pavel Burkov <pvburkov@gmail.com>
2020-09-07 17:34:24 +0300

    add sidebar menu, fix style for Notifications

--- src/modules/Workplace/Workplace.jsx
+++ src/modules/Workplace/Workplace.jsx
@@ -3,14 +3,24 @@ import {
   Link,
   Route,
   Switch,
-  useRouteMatch
+  useLocation,
+  useRouteMatch,
+  Redirect
 } from 'react-router-dom';
 import SplitterLayout from 'react-splitter-layout';
+import { withNaming } from '@bem-react/classname';
 import 'react-splitter-layout/lib/index.css';
 import './Workplace.css';
 
+const workplaceNavClasses = withNaming({
+  e: '__',
+  m: '--',
+  v: '-'
+})('workplace-navigation');
+
 const Workplace = () => {
   const match = useRouteMatch();
+  const { pathname } = useLocation();
 
   return (
     <SplitterLayout
@@ -18,15 +28,52 @@ const Workplace = () => {
       primaryMinSize={20}
       secondaryMinSize={20}
     >
-      <nav>
-        <Link to={`${match.url}/view`}>Просмотр</Link>
-        <Link to={`${match.url}/edit`}>Редактирование</Link>
-        <Link to={`${match.url}/about`}>О программе</Link>
+      <nav className={workplaceNavClasses()}>
+        <Link to={`${match.url}/view`}>
+          <div
+            className={workplaceNavClasses('link', {
+              active: pathname === `${match.url}/view`
+            })}
+          >
+            Просмотр
+          </div>
+        </Link>
+        <Link to={`${match.url}/edit`}>
+          <div
+            className={workplaceNavClasses('link', {
+              active: pathname === `${match.url}/edit`
+            })}
+          >
+            Редактирование
+          </div>
+        </Link>
+        <Link to={`${match.url}/about`}>
+          <div
+            className={workplaceNavClasses('link', {
+              active: pathname === `${match.url}/about`
+            })}
+          >
+            О программе
+          </div>
+        </Link>
+        <Link to={`${match.url}/exit`}>
+          <div
+            className={workplaceNavClasses('link', {
+              active: pathname === `${match.url}/exit`,
+              exit: true
+            })}
+          >
+            Выход
+          </div>
+        </Link>
       </nav>
       <Switch>
         <Route path={`${match.url}/view`} render={() => <h1>Просмотр</h1>} />
         <Route path={`${match.url}/edit`} render={() => <h1>Редактирование</h1>} />
         <Route path={`${match.url}/about`} render={() => <h1>О программе</h1>} />
+        <Route path={`${match.url}/exit`}>
+          <Redirect to="/" />
+        </Route>
       </Switch>
     </SplitterLayout>
   );
